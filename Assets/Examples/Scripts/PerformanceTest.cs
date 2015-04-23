8de8c36db80464ae062b5c1d43ce317053cb3815
Hakob <bboyhaks@gmail.com>
2015-04-23 22:13:59 +0400

    added performance test scene, tween package

---
+++ Assets/Examples/Scripts/PerformanceTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PerformanceTest : MonoBehaviour {
+
+	public int objectsCount = 100;
+	public float movingDuration = 2.0f;
+	public Transform objPref;
+
+	// Use this for initialization
+	void Start () {
+	
+		for(int i = 0; i < objectsCount; i++)
+		{
+			Transform obj = Instantiate(objPref) as Transform;
+			const float r = 10.0f;
+			Vector3 randPos = Random.insideUnitSphere*r;
+			CoolestTween.moveTo(obj,randPos,movingDuration,new CTweenConfig().setTweenType(CTweenType.PingPong));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
